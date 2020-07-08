9fd9531a0074979df8d76b95e96b3dc842ec2dad
Brad Kwon <brad@neuronation.de>
2020-07-08 23:55:05 +0200

    Working on Mapbox binding

---
+++ Kwon.Mapbox.Android/Kwon.Mapbox.Android.Core/Additions/Additions.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Mapbox.Android.Core
+{
+    public sealed partial class FileUtils
+    {
+        public sealed partial class LastModifiedComparator
+        {
+            public unsafe int Compare(Java.Lang.Object o1, Java.Lang.Object o2)
+            {
+                return Compare(o1 as Java.IO.File, o2 as Java.IO.File);
+            }
+        }
+    }
+}
---
+++ Kwon.Mapbox.Android/Kwon.Mapbox.Sdk.Geojson.Android/Additions/Additions.cs
@@ -0,0 +1,58 @@
+﻿
+namespace Mapbox.Geojson
+{
+    public partial class PointAsCoordinatesTypeAdapter
+    {
+        public override unsafe Java.Lang.Object Read(global::GoogleGson.Stream.JsonReader @in)
+        {
+            return ReadBase(@in) as Mapbox.Geojson.Point;
+        }
+
+        public override unsafe void Write(GoogleGson.Stream.JsonWriter @out, Java.Lang.Object value)
+        {
+            Write(@out, value as Mapbox.Geojson.Point);
+        }
+    }
+}
+
+namespace Mapbox.Geojson.Gson
+{
+    public partial class BoundingBoxTypeAdapter
+    {
+        public override unsafe Java.Lang.Object Read(GoogleGson.Stream.JsonReader @in)
+        {
+            return ReadBase(@in) as Mapbox.Geojson.BoundingBox;
+        }
+
+        public override unsafe void Write(GoogleGson.Stream.JsonWriter @out, Java.Lang.Object value)
+        {
+            Write(@out, value as Mapbox.Geojson.BoundingBox);
+        }
+    }
+
+    public partial class CoordinateTypeAdapter
+    {
+        public override unsafe Java.Lang.Object Read(GoogleGson.Stream.JsonReader @in)
+        {
+            return ReadBase(@in) as Android.Runtime.JavaList<Java.Lang.Double>;
+        }
+
+        public override unsafe void Write(GoogleGson.Stream.JsonWriter @out, Java.Lang.Object value)
+        {
+            Write(@out, value as Android.Runtime.JavaList<Java.Lang.Double>);
+        }
+    }
+
+    public partial class GeometryTypeAdapter
+    {
+        public override unsafe Java.Lang.Object Read(GoogleGson.Stream.JsonReader @in)
+        {
+            return ReadBase(@in) as Android.Runtime.JavaList<Mapbox.Geojson.IGeometry>;
+        }
+
+        public override unsafe void Write(GoogleGson.Stream.JsonWriter @out, Java.Lang.Object value)
+        {
+            Write(@out, value as Android.Runtime.JavaList<Mapbox.Geojson.IGeometry>);
+        }
+    }
+}
