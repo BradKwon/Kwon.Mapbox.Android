286ee063c77e83fc185fced93af06aa47ce545e1
Brad Kwon <brad@neuronation.de>
2020-07-18 21:06:26 +0200

    Finished Mapbox.Android.Sdk

---
+++ Kwon.Mapbox.Android/Kwon.Mapbox.Android.Sdk/Additions/Additions.cs
@@ -0,0 +1,21 @@
+﻿namespace Mapbox.Mapboxsdk.Utils
+{
+    public partial class FileUtils
+    {
+        public partial class CheckFileReadPermissionTask
+        {
+            protected override unsafe global::Java.Lang.Object DoInBackground(params Java.Lang.Object[] files)
+            {
+                return DoInBackground(files as global::Java.IO.File[]);
+            }
+        }
+
+        public partial class CheckFileWritePermissionTask
+        {
+            protected override unsafe global::Java.Lang.Object DoInBackground(params global::Java.Lang.Object[] files)
+            {
+                return DoInBackground(files as global::Java.IO.File[]);
+            }
+        }
+    }
+}
