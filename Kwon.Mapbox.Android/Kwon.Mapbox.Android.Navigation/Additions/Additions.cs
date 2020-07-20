9daaacb12adcb21ff2c29bb348051b71221f6317
Brad Kwon <brad@neuronation.de>
2020-07-20 19:46:33 +0200

    Finish updating Mapbox Navigation UI binding

---
+++ Kwon.Mapbox.Android/Kwon.Mapbox.Android.Navigation/Additions/Additions.cs
@@ -0,0 +1,10 @@
+﻿namespace Mapbox.Services.Android.Navigation.V5.Utils
+{
+    public partial class DownloadTask
+    {
+        protected override unsafe global::Java.Lang.Object DoInBackground(params global::Java.Lang.Object[] responseBodies)
+        {
+            return DoInBackground(responseBodies as global::Square.OkHttp3.ResponseBody[]);
+        }
+    }
+}
