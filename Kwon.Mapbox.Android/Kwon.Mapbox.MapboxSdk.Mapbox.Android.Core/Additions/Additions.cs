f6a41762242ae141a72e32897a4a17347900f902
Brad Kwon <junya815@gmail.com>
2021-02-06 22:42:48 +0100

    Updating SDKs

---
+++ Kwon.Mapbox.Android/Kwon.Mapbox.MapboxSdk.Mapbox.Android.Core/Additions/Additions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mapbox.Android.Core
+{
+    public sealed partial class FileUtils
+    {
+        public sealed partial class LastModifiedComparator
+        {
+            public int Compare (Java.Lang.Object o1, Java.Lang.Object o2)
+            {
+                return Compare(o1 as Java.IO.File, o2 as Java.IO.File);
+            }
+        }
+    }
+}
